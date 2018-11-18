e7d816058c2c6ac30d39f9fc59e1c5a4f4e1211e
Berflo26 <b.valleflores@gmail.com>
2018-11-18 14:09:29 -0600

    login

---
+++ Login/app.js
@@ -0,0 +1,11 @@
+function registrar(){
+    var correo = document.getElementById('correo').Value;
+    var contraseña = document.getElementById('contraseña').Value;
+
+    firebase.auth().createUserWithEmailAndPassword(correo, contraseña).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+}
